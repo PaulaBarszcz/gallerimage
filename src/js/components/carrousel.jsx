5b8b4b8e5bb22e4a40fdbd65418e2b0a2f94b6a6
PaulaBarszcz <paula.anna.barszcz@gmail.com>
2018-07-03 22:36:43 +0200

    basic react-slick carrousel works with images fetched from flickr

--- src/js/components/carrousel.jsx
+++ src/js/components/carrousel.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Slider from "react-slick";
-import {SingleSlide} from './singleSlide.jsx';
 
 class Carrousel extends React.Component {
   render() {
     var settings = {
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
@@ -16,32 +15,26 @@ class Carrousel extends React.Component {
         console.log('lala',photos[1].id);
 
     return (
-      <Slider {...settings}>
+      <div className="fetch-result-container">
+        <Slider {...settings}>
 
-        <div className="fetch-result-container">
-        
-          {this.props.photos.map(item => (
+          {this.props.photos.map((item,element) => (
             this.imgid = item.id,
-            console.log('imgid',this.imgid),
             this.userid = item.owner,
-
-            
-
             this.imgfarm = item.farm,
             this.imgserver = item.server,
             this.imgsecret = item.secret,
-            this.imgsrc = `https://farm${this.imgfarm}.staticflickr.com/${this.imgserver}/${ this.imgid}_${this.imgsecret}.jpg`,
-            console.log('this.imgsrc',this.imgsrc),
+            this.imgsrc = `https://farm${this.imgfarm}.staticflickr.com/${this.imgserver}/${this.imgid}_${this.imgsecret}.jpg`,
 
-            <SingleSlide key={item.id} imgsrc={this.imgsrc} />
-              
-         
+            <div key={item.id}>
+                <h3>Dog picture #{element+1}/100</h3>
+                <img src={this.imgsrc} alt="dog image" />
+            </div>
           ))}
-        </div>
 
-     
-       
-      </Slider>
+        </Slider>
+      </div>
+
     );
   }
 }
