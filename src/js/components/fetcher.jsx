694cef91e7b444dbfa29a11b6f75d5d16c9226b1
PaulaBarszcz <paula.anna.barszcz@gmail.com>
2018-07-18 20:46:37 +0200

    increased resource-saving - decreased frequency of state update during user-info-fetching

--- src/js/components/fetcher.jsx
+++ src/js/components/fetcher.jsx
@@ -15,64 +15,55 @@ class Fetcher extends React.Component {
 
   componentDidMount() {
     fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5530b236f1442ae162669f80660df592&tags=dog&text=dog&format=json&nojsoncallback=1")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.allPhotos = result.photos.photo;
-          this.setState({
-            isLoaded: true,
-            photos: result.photos.photo
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-      .then(
-        (resultusers) =>  {   
-        console.log(this.allPhotos);
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.allPhotos = result.photos.photo;
+        this.setState({
+          isLoaded: true,
+          photos: result.photos.photo
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+    .then(
+      (resultusers) =>  {   
+      console.log(this.allPhotos);
 
-        let photoInfoArray = [];
+      let photoInfoArray = [];
 
-        for (var i=0; i<this.allPhotos.length; i++) {
+      for (var i=0; i<this.allPhotos.length; i++) {
 
-          // old https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=5530b236f1442ae162669f80660df592&user_id=${this.allPhotos[i].owner}&format=json&nojsoncallback=1&auth_token=72157670792491088-fecc059fbd3d44cb&api_sig=8545aa490a24023369ac23219cb1bc90
+        fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=5530b236f1442ae162669f80660df592&user_id=${this.allPhotos[i].owner}&format=json&nojsoncallback=1`)
+        .then(res => res.json())
+        .then(
+          (resultusers) => {
+            let desc = Object.values(resultusers.person.description)[0];
+            let username = Object.values(resultusers.person.username)[0];
+            let date = Object.values(resultusers.person.photos.firstdatetaken)[0];
+            photoInfoArray.push({"desc": desc, "username": username, "date": date});
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        ) 
+      }
 
-          // new https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=5530b236f1442ae162669f80660df592&format=json&nojsoncallback=1
-
-          fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=5530b236f1442ae162669f80660df592&user_id=${this.allPhotos[i].owner}&format=json&nojsoncallback=1`)
-            .then(res => res.json())
-            .then(
-              (resultusers) => {
-
-                let desc = Object.values(resultusers.person.description)[0];
-                let username = Object.values(resultusers.person.username)[0];
-                let date = Object.values(resultusers.person.photos.firstdatetaken)[0];
-
-                photoInfoArray.push({"desc": desc, "username": username, "date": date});
-
-                this.setState({
-                  isLoaded: true,
-                  photoInfo: photoInfoArray
-                });
-              },
-              (error) => {
-                this.setState({
-                  isLoaded: true,
-                  error
-                });
-              }
-          )
-          
-        }
-
-        return resultusers
-
+      this.setState({
+        isLoaded: true,
+        photoInfo: photoInfoArray
       })
 
+      return resultusers
+    })
   }
 
   render() {
